66f53532e953b21c2130d57e4ee6a39d35321080
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-05 10:39:45 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        enum Months {Январь, Февраль, Март, Апрель, Май, Июнь, Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь}
+        static void Main(string[] args)
+        {
+            foreach (int i in Enum.GetValues(typeof(Months)))
+            {
+                Console.WriteLine($"{i}  {Enum.GetName(typeof(Months), i)}");
+            }
+
+        }
+    }
+}
