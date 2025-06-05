cd4bfc52781c4a935571bd2ba9817a77d5cd5200
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-05 10:56:46 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    enum UserRole { Администратор, Модератор, Пользователь, Гость }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите роль пользователя (Администратор, Модератор, Пользователь, Гость):");
+            string input = Console.ReadLine();
+            UserRole role;
+            try
+            {
+                role = (UserRole)Enum.Parse(typeof(UserRole), input, true);
+                Console.WriteLine($"Вы зарегестрировались как: {role}");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Ошибка: введена некорректная роль пользователя.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
