140bf0c306831b4599e452450958479f1d0e4ded
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-05 10:42:25 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    enum Days
+    {
+        Sat, Sun, Mon, Tue, Wed, Thu, Fri
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Days i;
+        for (i = Days.Sat; i <= Days.Fri; i++)
+            Console.WriteLine(i + " имеет значение " + (int)i);
+        Console.Read();
+    }
+}
+    
+
